a345d03897eaa800e1934f41db688e39675e5629
Shahria-Ashraf-Laam <shahria.ashraf45@gmail.com>
2026-03-11 00:55:03 +0600

    homepage design completed and API connected

---
+++ scripts/homepage.js
@@ -0,0 +1,151 @@
+// console.log("Hello from homepgae!");
+
+let activeTab = "all"
+let issuesData = [];
+
+const api = "https://phi-lab-server.vercel.app/api/v1/lab/issues";
+
+const displayIssueDetails = (issue) => {
+
+  // console.log(issue);
+  const detailsBox = document.getElementById("details-container");
+  detailsBox.innerHTML = `
+                    <div>
+                        <h1 class="font-bold text-lg">${issue.title}</h1>
+                    </div>
+                     <div class="flex gap-4 items-center">
+                    <button class="btn btn-soft rounded-full">${issue.status === "open" ? "Opened" : "Closed"}</button>
+                    <div class="flex gap-4">
+                     <p>Opened by ${issue.author}</p>
+                     <p>${new Date(issue.createdAt).toLocaleDateString()}</p>
+                     </div>
+                    </div>
+                    <div>
+                     ${issue.labels.map(label => `<button class="btn btn-soft btn-secondary rounded-full border">${label.toUpperCase()}</button>`).join(" ")}
+                    </div>
+                     <div>
+                     <p>${issue.description}</p>
+                    </div>
+        <div class="flex p-4 bg-[#F8FAFC] items-center gap-5 rounded-md">
+            <p>Assignee: <span class="font-bold">${issue.assignee}</span></p>
+            <p>Priority: <button class="btn btn-soft rounded-full">${issue.priority.charAt(0).toUpperCase() + issue.priority.slice(1)}</button></p>
+        </div>
+    `;
+};
+
+
+//Loading Cards
+
+function showIssues(issues) {
+  const issuesContainer = document.getElementById("issues-container");
+  issuesContainer.innerHTML = "";
+
+  // Filter issue
+  let filteredIssues;
+
+  if (activeTab === "all") {
+    filteredIssues = issues;
+  } else {
+    filteredIssues = issues.filter(function (issue) {
+      return issue.status === activeTab;
+    });
+  }
+
+  const issueCount = document.getElementById("issue-count");
+  issueCount.innerText = `${filteredIssues.length} Issues`;
+
+  for (let issue of issues) {
+    // console.log(issue)
+    const statusImg = issue.status === "open"
+      ? "assets/Open-Status.png"
+      : "assets/Closed-Status.png";
+
+    const issueCard = document.createElement("div");
+
+    // Adding border top dynamically
+    issueCard.style.borderTop = issue.status === "open" ? "4px solid #00A96E" : "4px solid #A855F7";
+    issueCard.style.borderRadius = "0.5rem";
+
+    // Adding labels dynamically
+    const labelAdd = issue.labels.map(label => `
+                <button class="btn btn-soft btn-secondary rounded-full border">${label.toUpperCase()}</button>
+            `).join(" ")
+
+
+    issueCard.innerHTML = `
+                <div class="issue-card space-y-4 shadow-sm p-4 rounded-lg">
+
+                    <!-- card status img -->
+                    <div class="card-img-priority flex flex-1 justify-between items-center">
+                        <div>
+                            <img width="30px" src="${statusImg}" alt="">
+                        </div>
+
+                        <button class="btn btn-soft btn-secondary rounded-full">${issue.priority}</button>
+                    </div>
+
+                    <!-- card header -->
+                     <div class="card-header space-y-2">
+                        <h2 class="font-semibold">${issue.title}</h2>
+                        <p class="text-[#64748B] text-[14px]">${issue.description}</p>
+                     </div>
+
+                     <!-- topics name -->
+                      ${labelAdd}
+
+                      <!-- date -->
+
+                      <div class="date py-4 border-t border-[#64748B]">
+                        <p class="text-[#64748B]">#${issue.id} by ${issue.author}</p>
+                        <p class="text-[#64748B]">${new Date(issue.createdAt).toLocaleDateString()}</p>
+                      </div>
+
+
+                </div>
+        `
+
+    issueCard.addEventListener("click", () => loadIssueDetail(issue.id));
+    issuesContainer.append(issueCard);
+  }
+}
+
+
+// Tab-buttons
+const tabButtons = document.querySelectorAll(".issue-tab-btn");
+tabButtons.forEach(btn => {
+  btn.addEventListener("click", async () => {
+    activeTab = btn.dataset.tab;
+
+    tabButtons.forEach(b => {
+      b.classList.remove("btn-primary");
+      b.classList.add("btn-outline");
+    });
+
+    btn.classList.remove("btn-outline");
+    btn.classList.add("btn-primary");
+
+    if (activeTab === "all") {
+      showIssues(issuesData);
+    } else {
+      const filtered = issuesData.filter(issue => issue.status === activeTab);
+      showIssues(filtered);
+    }
+  })
+})
+
+
+document.getElementById("btn-search").addEventListener("click", async () => {
+  const input = document.getElementById("input-search");
+  const searchValue = input.value.trim().toLowerCase();
+
+  const res = await fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues");
+  const data = await res.json();
+
+  const allIssues = data.data;
+  const filtered = allIssues.filter(issue =>
+    issue.title.toLowerCase().includes(searchValue) ||
+    issue.description.toLowerCase().includes(searchValue)
+  );
+
+  showIssues(filtered);
+});
