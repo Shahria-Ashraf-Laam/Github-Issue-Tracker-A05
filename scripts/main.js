80661b50fb82bb70659bcab7c9bced260c92754d
Shahria-Ashraf-Laam <shahria.ashraf45@gmail.com>
2026-03-10 16:23:12 +0600

    applied JS to sign-in button and created homepage file

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,22 @@
-console.log("hello");
+// console.log("hello");
+
+document.getElementById("sign-btn").addEventListener("click", () => {
+
+  // Getting username and password from input fields
+  const usernameInput = document.getElementById("username-input");
+  const username = usernameInput.value;
+
+  const passInput = document.getElementById("password-input");
+  const password = passInput.value;
+
+  // Checking username and password
+  if (username === "admin" && password === "admin123") {
+    alert("Login Successful");
+    window.location.assign("homepage.html");
+  }
+  else {
+    alert("Login Failed, Try Again!");
+    return;
+  }
+
+})
