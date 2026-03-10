dbd38923cacd98082bc81f94257637423352f95c
Shahria-Ashraf-Laam <shahria.ashraf45@gmail.com>
2026-03-11 01:17:50 +0600

    spinner added

--- scripts/homepage.js
+++ scripts/homepage.js
@@ -5,6 +5,30 @@ let issuesData = [];
 
 const api = "https://phi-lab-server.vercel.app/api/v1/lab/issues";
 
+
+//Spinner
+const manageSpinner = (status) => {
+  if (status == true) {
+    document.getElementById("spinner").classList.remove("hidden");
+    document.getElementById("issue-container").classList.add("hidden");
+  } else {
+    document.getElementById("issue-container").classList.remove("hidden");
+    document.getElementById("spinner").classList.add("hidden");
+  }
+}
+
+manageSpinner(true);
+fetch(api)
+  .then(res => res.json())
+  .then(data => {
+    issuesData = data.data;
+    showIssues(issuesData);
+    manageSpinner(false);
+  })
+
+//Spinner end
+
+
 const displayIssueDetails = (issue) => {
 
   // console.log(issue);
